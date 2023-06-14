1ec7132faf8caa1025e63eb5f15db93302fea251
sangdob <51118110+sangdob@users.noreply.github.com>
2023-06-15 01:25:54 +0900

    Revert "Feture UI 양식"

--- src/main/java/likelion15/mutsa/repository/HelloRepositoryImpl.java
+++ src/main/java/likelion15/mutsa/repository/HelloRepositoryImpl.java
@@ -1,15 +1,11 @@
 package likelion15.mutsa.repository;
 
 import likelion15.mutsa.dto.HelloResponse;
-import likelion15.mutsa.dto.QHelloResponse;
 import likelion15.mutsa.entity.Hello;
-import likelion15.mutsa.entity.QHello;
 import likelion15.mutsa.repository.support.Querydsl4RepositorySupport;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
-import static likelion15.mutsa.entity.QHello.*;
 
 @Repository
 public class HelloRepositoryImpl extends Querydsl4RepositorySupport implements HelloRepositoryCustom {
@@ -19,8 +15,8 @@ public class HelloRepositoryImpl extends Querydsl4RepositorySupport implements H
 
     @Override
     public List<HelloResponse> findByHello() {
-        return select(new QHelloResponse(hello.name))
-                .from(hello)
-                .fetch();
+        return null;
+//        return select(new HelloResponse(hello.name))
+//                .from()
     }
 }
