7c1a9d54a6d1d3c257b501a319be97f75ce0f97f
kwonghyun <momky4@naver.com>
2023-06-25 12:30:04 +0900

    yml gitignore 추가 되는지 확인

--- src/main/java/likelion15/mutsa/entity/enums/UserAuth.java
+++
@@ -1,8 +0,0 @@
-package likelion15.mutsa.entity.enums;
-
-import lombok.Getter;
-
-@Getter
-public enum UserAuth {
-    ADMIN, USER, MANAGER, MENTO;
-}
--- src/main/java/likelion15/mutsa/repository/HelloRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package likelion15.mutsa.repository;
-
-import likelion15.mutsa.dto.HelloResponse;
-
-import java.util.List;
-
-public interface HelloRepositoryCustom {
-    List<HelloResponse> findByHello();
-}
