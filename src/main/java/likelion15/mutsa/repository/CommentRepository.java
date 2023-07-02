03324d297c575b59db852c1d6520acc2095bd100
kwonghyun <momky4@naver.com>
2023-07-02 16:09:28 +0900

    refactor: 글, 댓글 조회시 디테일 추가 - DeletedStatus.NONE 인거만 조회 - 좋아요 글/댓글 조회시 isLike.YES인거만 조회

--- src/main/java/likelion15/mutsa/repository/CommentRepository.java
+++ src/main/java/likelion15/mutsa/repository/CommentRepository.java
@@ -2,28 +2,35 @@ package likelion15.mutsa.repository;
 
 import likelion15.mutsa.entity.Comment;
 import likelion15.mutsa.entity.enums.DeletedStatus;
+import likelion15.mutsa.entity.enums.YesOrNo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     //userId로 작성한 게시물 전체 조회
-    Page<Comment> findAllByUsername(String username, Pageable pageable);
+    Page<Comment> findAllByUsernameAndIsDeleted(String username, DeletedStatus isDeleted, Pageable pageable);
+
+    default Page<Comment> findAllByUsernameAndIsDeleted(String username, Pageable pageable) {
+        return findAllByUsernameAndIsDeleted(username, DeletedStatus.NONE, pageable);
+    }
 
     // userId로 좋아요한 게시물 전체 조회
-    @Query("SELECT c FROM Comment c JOIN FETCH c.likes l WHERE c.username = :username and c.isDeleted = :isDeleted")
-    Page<Comment> findAllByUsernameWithLikes(@Param("username") String username, @Param("isDeleted") DeletedStatus deletedStatus, Pageable pageable);
+    @Query("SELECT c " +
+            "from Comment c " +
+            "join fetch Likes l on c.id = l.comment.id " +
+            "WHERE l.user.id = ?1 and c.isDeleted = ?2 and l.isLike = ?3")
+    Page<Comment> findAllByUsernameWithLikes(Long userId, DeletedStatus isDeleted, YesOrNo isLike, Pageable pageable);
 
-    default Page<Comment> findAllByUsernameWithLikes(String username, Pageable pageable) {
-        return findAllByUsernameWithLikes(username, DeletedStatus.NONE, pageable);
+    default Page<Comment> findAllByUsernameWithLikes(Long userId, Pageable pageable) {
+        return findAllByUsernameWithLikes(userId, DeletedStatus.NONE, YesOrNo.YES, pageable);
     }
 
     // 댓글 삭제
-    @Query("update Comment c set c.isDeleted = :isDeleted where c.id = :commentId")
-    void updateIsDeletedById(@Param("commentId") Long commentId, @Param("isDeleted") DeletedStatus deletedStatus);
+    @Query("update Comment c set c.isDeleted = ?2 where c.id = ?1")
+    void updateIsDeletedById(Long commentId, DeletedStatus isDeleted);
 
     default void updateIsDeletedById(Long commentId) {
         updateIsDeletedById(commentId, DeletedStatus.DELETE);
