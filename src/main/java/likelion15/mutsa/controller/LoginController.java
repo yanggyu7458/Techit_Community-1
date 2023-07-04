815b2b70682415c2e6330151a1693447d541828b
dani <joyjoy312@naver.com>
2023-07-04 11:28:22 +0900

    - 세션리스트 확인하는 코드 추가 /session-list에서 로그인한 사용자의 sessionId와 uuid정보를 확인할 수 있다.

--- src/main/java/likelion15/mutsa/controller/LoginController.java
+++ src/main/java/likelion15/mutsa/controller/LoginController.java
@@ -11,11 +11,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,6 +27,9 @@ public class LoginController {
 
     private final LoginService loginService;
     private final JoinService joinService;
+
+    // 세션리스트 확인용
+    private static Hashtable sessionList = new Hashtable();
 
     // 로그인 페이지
     @GetMapping("/login")
@@ -51,8 +56,13 @@ public class LoginController {
             SessionDto sessionDto = loginService.createSessionDto(user);
 
             // 세션에 sessionDto를 넣어줌
-            session.setAttribute("sessionDto", sessionDto);
+            session.setAttribute("uuid", sessionDto);
             session.setMaxInactiveInterval(1800); // 1800=30분
+
+            // 세션을 세션리스트에 추가(세션 리스트 확인용)
+            // getid가 아니고 uuid 추가해야하는지 알아보고 수정하기[ ]
+            sessionList.put(session.getId(), session);
+
             return "redirect:/home";
         } else { // 로그인 실패
             if(!joinService.IsExistEmail(joinDto.getEmail())){
@@ -70,6 +80,9 @@ public class LoginController {
         //세션이 없으면 null return
         HttpSession session = request.getSession(false);
         if (session != null) {
+            // 로그아웃 시 세션 리스트에서도 제거
+            sessionList.remove(session.getId());
+
             session.invalidate();
         }
 
@@ -77,12 +90,31 @@ public class LoginController {
     }
     @GetMapping("/home")
     public String home(Model model,
-                       @SessionAttribute(name="sessionDto",required = false) SessionDto sessionDto){
+                       @SessionAttribute(name="uuid",required = false) SessionDto sessionDto){
 
         if ((sessionDto != null)) {
             model.addAttribute("name", sessionDto.getName());
         }
 
         return "home";
+    }
+    // 세션 리스트 확인용
+    // {세션id : uuid, 세션id, uuid,..} 형태로 로그인한 유저확인
+    @GetMapping("/session-list")
+    @ResponseBody
+    public Map<String,String> sessionList(){
+        Enumeration elements = sessionList.elements();
+        Map<String,String> lists = new HashMap<>();
+        while (elements.hasMoreElements()) {
+
+            HttpSession session = (HttpSession) elements.nextElement();
+
+            try{
+                lists.put(session.getId(), ((SessionDto) session.getAttribute("uuid")).getUuid());
+            }catch (IllegalStateException e){
+                // 세션이 이미 무효화된 경우, 해당 세션은 무시하고 다음 세션으로 진행
+            }
+        }
+        return lists;
     }
 }
